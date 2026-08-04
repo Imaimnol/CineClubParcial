1d9df66c0d9596365b190d91ab4b828746a2d14a
Imaimnol <imanolreyes02@gmail.com>
2026-08-04 18:47:00 -0300

    Configura variables de entorno y middlewares del servidor

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
     res.send("Servidor funcionando");
